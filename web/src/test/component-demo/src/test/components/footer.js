69e622d4ff50154483ba8911b0169b1caf1170f0
abeobk <abeobk@gmail.com>
2025-09-22 10:58:32 +0900

    update

--- web/src/test/component-demo/src/test/components/footer.js
+++
@@ -1,9 +0,0 @@
-const FooterComponent = {
-    render: function(text = '© 2025 My App. All rights reserved.') {
-        return `
-            <footer class="footer">
-                <p>${text}</p>
-            </footer>
-        `;
-    }
-};
